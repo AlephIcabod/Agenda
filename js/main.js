39075ec5e0c6e0a80464bd25b17d845b5b01fd3e
Miguel Bautista <angel_507@live.com.mx>
2016-08-18 01:18:14 -0500

    terminado

--- js/main.js
+++ js/main.js
@@ -22,59 +22,17 @@ var app = angular.module("app", ["ngRoute"])
 	this.data = localStorage.getItem("agendaData");
 	if (this.data != null) {
 		$scope.contacts = [];
-		var rawContacts = JSON.parse(data);
+		var rawContacts = JSON.parse(control.data);
 		_(rawContacts)
 			.forEach(function (value) {
-				control.contacts.push(new Contact(value));
+				$scope.contacts.push(new Contacto(value));
 			});
 	} else {
 		$scope.contacts = [new Contacto({
 				name: "miguel",
 				last_name: "Bautista",
 				email: "angel.com",
-				country: "mexico"
-			}),
-            new Contacto({
-				name: "angel",
-				last_name: "Cruz",
-				email: "angel.com",
-				country: "españa"
-			}),
-            new Contacto({
-				name: "reyna",
-				last_name: "Bautista",
-				email: "reyna.com",
-				country: "mexico"
-			}),
-            new Contacto({
-				name: "lety",
-				last_name: "ramirez",
-				email: "lety.com",
-				country: "mexico"
-			}),
-			new Contacto({
-				name: "miguel",
-				last_name: "Bautista",
-				email: "angel.com",
-				country: "mexico"
-			}),
-	            new Contacto({
-				name: "angel",
-				last_name: "Cruz",
-				email: "angel.com",
-				country: "españa"
-			}),
-	            new Contacto({
-				name: "reyna",
-				last_name: "Bautista",
-				email: "reyna.com",
-				country: "mexico"
-			}),
-	            new Contacto({
-				name: "lety",
-				last_name: "ramirez",
-				email: "lety.com",
-				country: "mexico"
+				country: "mx"
 			})
         ];
 	}
@@ -83,6 +41,8 @@ var app = angular.module("app", ["ngRoute"])
 
 .controller("listController", ["$scope", "$location", function ($scope, $location) {
 	control = this;
+
+	$scope.verBotones = false;
 	$scope.$parent.listView = true;
 	$scope.$parent.contactView = false;
 	this.form = function (id) {
@@ -108,7 +68,69 @@ var app = angular.module("app", ["ngRoute"])
 		//	this.parent.$scope.listView = true;
 }])
 
-.controller("contactController", ["$scope", function ($scope) {
+.controller("contactController", ["$scope", "$routeParams", "$location", function ($scope, $routeParams, $location) {
+
 	$scope.$parent.listView = false;
 	$scope.$parent.contactView = true;
+	control = this;
+	this.countries=window.countries;
+	this.nuevo=true;
+
+	var id = Number($routeParams.id)
+	if (!_.isNaN(id)) {
+		control.contact = _.find($scope.contacts, function (i) {
+			return i.id == id
+		})
+		this.currentContact = angular.copy(this.contact);
+		control.nuevo=false;
+	}
+	this.add=function(){
+		console.log("añadir")
+			var auxCont=new Contacto(control.currentContact);
+			$scope.contacts.push(auxCont);
+			control.goHome();
+		}
+		this.update=function(){
+			_.forIn(control.currentContact,function(val,key){
+				control.contact[key]=val;
+			})
+			control.goHome();
+		}
+		this.delete=function(){
+			var deleted=_.remove($scope.contacts,function(obj){
+				return obj.id==control.currentContact.id;
+			});
+			control.goHome();
+		}
+
+		this.goHome=function(){
+			control.persistir();
+			$location.path("/");
+		}
+		this.persistir=function(){
+			localStorage.setItem("agendaData",JSON.stringify($scope.contacts));
+		}
+
+}])
+
+.directive("listReady", ["$timeout", function ($timeout) {
+	return {
+		restrict: "A",
+		link: function ($scope, elem, attr) {
+			if ($scope.$last === true) {
+				console.log($scope);
+				$timeout(function () {
+					var wrap = document.getElementsByClassName('list-wrapper')[0];
+					var ul = wrap.getElementsByTagName("ul")[0];
+					if (ul.offsetHeight > wrap.offsetHeight) {
+						console.log("mostrar")
+						$scope.$parent.verBotones = true;
+						console.log($scope.$parent.verBotones)
+					} else {
+						$scope.$parent.verBotones = false;
+					}
+				}, 10);
+			}
+		}
+	}
 }]);
